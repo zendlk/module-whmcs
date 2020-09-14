68e4844888f935f2f71758378a3371f868534caa
Shyamin Ayesh <me@shyamin.com>
2020-09-14 15:21:43 +0530

    create some schema and insert data into them

--- zend.php
+++ zend.php
@@ -1,5 +1,8 @@
 <?php
 
+// include_once "module/Dispatcher.php";
+use WHMCS\Database\Capsule;
+
 /**
  * Terminate the process if this script is called outside
  * of WHMCS system.
@@ -46,10 +49,62 @@ function zend_config() {
  * to bootstrap module and migrate any database schemas.
  */
 function zend_activate() {
-	return [
-		"status" => "success",
-		"description" => "Zend module successfully activated"
-	];
+
+	try {
+
+		/**
+		 * Create database schemas as per our need on the
+		 * activation method. Followings are the schemas
+		 * created during this activation process.
+		 *
+		 *    - mod_zend_history
+		 *    - mod_zend_templates
+		 *
+		 */
+		Capsule::schema()->create(
+			'mod_zend_history',
+			function($schema) {
+				$schema->increments('id');
+				$schema->integer('client_id');
+				$schema->integer('phone');
+			}
+		);
+
+		Capsule::schema()->create(
+			'mod_zend_templates',
+			function($schema) {
+				$schema->increments('id');
+				$schema->string('name');
+				$schema->enum('type', ['admin', 'client', 'invoice', 'ticket']);
+				$schema->text('message');
+				$schema->boolean('is_active');
+			}
+		);
+
+		/**
+		 * We also need to seed some important data into
+		 * the template schema. Administrator need to have
+		 * some predefined templates to work with.
+		 */
+		Capsule::table('mod_zend_templates')->insert([
+			"name"		=> "On_Register",
+			"type"		=> "client",
+			"message"	=> "Hi {firstname}, Welcome to our website."
+		]);
+
+		return [
+			"status" => "success",
+			"description" => "Zend module successfully activated"
+		];
+
+	} catch (\Exception $e) {
+
+		return [
+            "status" => "error",
+            "description" => "Unable to create schema: ".$e->getMessage()
+        ];
+
+	}
 }
 
 
@@ -60,18 +115,46 @@ function zend_activate() {
  * things from the WHMCS system.
  */
 function zend_deactivate() {
-	return [
-		"status" => "success",
-		"description" => "Zend module successfully deactivated"
-	];
+
+	try {
+
+		Capsule::schema()->dropIfExists('mod_zend_history');
+		Capsule::schema()->dropIfExists('mod_zend_templates');
+
+		return [
+			"status" => "success",
+			"description" => "Zend module successfully deactivated"
+		];
+
+	} catch (\Exception $e) {
+		return [
+			"status" => "error",
+			"description" => "Unable to drop schema".$e->getMessage()
+		];
+	}
+
 }
+
 
 
 function zend_output($vars) {
 
+	/**
+	 * Identify the requested tab and dispatch the request
+	 * according to the tab controller.
+	 */
+	// Dispatcher::test("|test");
+
+
 	echo "<pre>";
 		var_dump( $vars );
+		var_dump( $_REQUEST['tab'] );
 	echo "</pre>";
+
+	// $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
+	// $dispatcher = new AdminDispatcher();
+	// $response = $dispatcher->dispatch($action, $vars);
+	// echo $response;
 
 }
 
