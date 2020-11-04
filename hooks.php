615088ae8504aed3ac99c3fb8f851d229cf5dfb5
Shyamin Ayesh <me@shyamin.com>
2020-11-05 02:39:28 +0530

    hooks initializer

---
+++ hooks.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!defined("WHMCS")) die("This file cannot be accessed directly");
+
+/**
+ * Include Zend PHP SDK to perform actions when there is new
+ * event happend from action hook.
+ */
+include_once dirname(__FILE__)."/libs/zendlk/php-sdk/support/Config.php";
+include_once dirname(__FILE__)."/libs/zendlk/php-sdk/api/messages.php";
+
+
+/**
+ * We need to get a list of items in the hooks directory
+ * and then load them into the WHMCS known space to run
+ * them according to the WHMCS.
+ */
+include_once dirname(__FILE__)."/hooks/invoices/created.php";
+include_once dirname(__FILE__)."/hooks/tickets/opened.php";
+
+?>
