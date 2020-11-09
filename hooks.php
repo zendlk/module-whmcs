2ec3910a4536f505b2496c3122de815d9cc1d3b9
Shyamin Ayesh <me@shyamin.com>
2020-11-09 18:01:01 +0530

    move library files into src directory

--- hooks.php
+++ hooks.php
@@ -6,8 +6,8 @@ if (!defined("WHMCS")) die("This file cannot be accessed directly");
  * Include Zend PHP SDK to perform actions when there is new
  * event happend from action hook.
  */
-include_once dirname(__FILE__)."/libs/zendlk/php-sdk/support/Config.php";
-include_once dirname(__FILE__)."/libs/zendlk/php-sdk/api/SMS/Message.php";
+include_once dirname(__FILE__)."/libs/zendlk/php-sdk/src/support/Config.php";
+include_once dirname(__FILE__)."/libs/zendlk/php-sdk/src/api/SMS/Message.php";
 
 
 /**
